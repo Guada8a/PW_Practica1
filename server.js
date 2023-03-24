0a38af27106fe53ca6adafa680a406001fd573c4
Guadalupe Ochoa <jochoa10@ucol.mx>
2023-03-24 08:28:15 -0600

    add practice15

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express'); //Inyección de dependencias
+let app = express(); //Instancia de express
+let PORT = process.env.PORT || 3000; //Definición de puerto de escucha
+app.use('/assets', express.static(__dirname + '/public')); //contenido estático
+
+app.use(express.urlencoded({ extended: false }));
+// * Agregar la siguiente línea para usar ejs
+app.set('view engine', 'ejs');
+
+
+//Crear la ruta Student
+app.get('/student', function (req, res) {
+    let t = req.params.id;
+    res.render('student');
+})
+// * Se agrega la ruta /addStudent para mostrar el formulario de agregar estudiante
+/*
+app.get('/addStudent', function (req, res) {
+    res.send(` Nombre: ${req.body.nombre}
+            <br> Edad: ${req.body.edad}
+            <br> NSS: ${req.body.nss}
+            <br> Tipo de sangre: ${req.body.tipoSangre}`);
+});
+*/
+// * Se agrega la ruta /addStudent para mostrar el formulario de agregar estudiante
+app.post('/addStudent', function (req, res) {
+    res.render('displayData',{
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        nss: req.body.nss,
+        tipoSangre: req.body.tipoSangre
+    });
+});
+
+app.listen(PORT);
