3ee8b9e410154cdd78a1c14baef309784fdf113c
Guadalupe Ochoa <jochoa10@ucol.mx>
2023-03-30 07:26:51 -0600

    add student views [forms, and ejs files], update of server.js on branch main

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 let express = require('express');
+let path = require('path');
 //Inyección de la dependencia de express
 let app = express();
 //App que hace la función de servidor
 let personsRoute = require('./routes/person');
+let studentRoute = require('./routes/student');
+console.log(studentRoute);
 //Incluimos el router de person
 
-app.set('view engine', 'ejs');
-app.use(personsRoute);
-app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.json()); //Para poder parsear JSON (application/json
+app.use('/assets', express.static(path.join(__dirname + '../public'))); //contenido estático
+
+
+// * NOTA: La siguiente línea es para indicar que se va a Parsear las peticiones con URL Encoded
+app.use(express.urlencoded({ extended: false }));
 
 let PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(personsRoute);
+app.use(studentRoute);
 //Definición de escucha
 app.listen(PORT, function () {
     console.log('Server running on port: ' + PORT);
