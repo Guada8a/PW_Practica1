f9c3c55dceb7517a7f4afc75a85597dbb3168bd9
Guadalupe Ochoa <jochoa10@ucol.mx>
2023-03-30 21:36:45 -0600

    NEW: connection /personJson with endpoint displayData1 and the terminal

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,10 +1,12 @@
 const express = require('express');
+//En este apartado se crea el router, y la ruta /student
 let router = express.Router();
 router.get('/student', (req, res) => {
     let t = req.params.id
     res.render('student');
 });
 
+//En este apartado se crea la ruta /addStudent y se le pasa los datos del formulario
 router.post('/addStudent', (req, res) => {
     res.render('displayData', { nombre: req.body.nombre,
                                 edad: req.body.edad,
@@ -13,10 +15,25 @@ router.post('/addStudent', (req, res) => {
     });
 });
 
+//En este apartado se crea la ruta /personJson y se le pasa los datos del formulario
 router.post('/personJson', express.json({ type: '*/*' }), (req, res) => {
-    console.log(`nombre: ${req.body.nombre} | apellido: ${req.body.apellido}`);
+    let name = req.body.fname === undefined ? ' ' : `nombre: ${req.body.fname}`;
+    let lastname = req.body.lname === undefined ? '' : `| apellido: ${req.body.lname}`;
+    
+    let nombre = req.body.nombre === undefined ? '' : `nombre: ${req.body.nombre}`;
+    let apellido = req.body.apellido === undefined ? '' : `| apellido: ${req.body.apellido}`;
+
+    console.log(name, lastname);
+    console.log(nombre, apellido);
+    let t = req.params.id;
+    res.render('displayData1',
+        {
+            nombre: req.body.fname,
+            apellido: req.body.lname
+        });
 });
 
+//En este apartado se crea la ruta /testJson y se le pasa los datos del formulario
 router.get('/testJson', (req, res) => {
     res.render('testJson');
 });
