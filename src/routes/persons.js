33dd98acb0fb83a41b234b4b6a8d733031e7d161
Guadalupe Ochoa <jochoa10@ucol.mx>
2023-05-04 21:10:50 -0600

    practice 20b: update and delete

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -26,5 +26,47 @@ router.post('/addPerson', async (req, res) => {
     //Guardar en la base de datos con save()
     newPerson.save().then((data) => res.redirect('/gente')).catch((err) => res.json(err));
 })
+
+//Editar una persona por su id
+router.get('/updatePerson/:id', async (req, res) => {
+    try {
+        const person = await Person.findById(req.params.id);
+        if (!person) throw new Error('No se encontró la persona');
+        res.render('updatePerson', { person });
+    } catch (error) {
+        console.error(error);
+        res.status(404).send(error.message);
+    }
+});
+
+//Actualizar una persona por su id
+router.post('/updatePerson/:id', async (req, res) => {
+    try {
+        const person = await Person.findByIdAndUpdate(req.params.id, {
+            nombre: req.body.nombre,
+            edad: req.body.edad,
+            tipoSangre: req.body.tipoSangre,
+            nss: req.body.nss
+        });
+        if (!person) throw new Error('No se encontró la persona');
+        res.redirect('/gente');
+    } catch (error) {
+        console.error(error);
+        res.status(404).send(error.message);
+    }
+});
+
+//Eliminar una persona por su id
+router.get('/deletePerson/:id', async (req, res) => {
+    try {
+        const person = await Person.findByIdAndDelete(req.params.id);
+        if (!person) throw new Error('No se encontró la persona');
+        res.redirect('/gente');
+    } catch (error) {
+        console.error(error);
+        res.status(404).send(error.message);
+    }
+});
+
 //Exportar el modulo
 module.exports = router;
